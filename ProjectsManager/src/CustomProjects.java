2662758854bba7f27ce6255a84514d1d5f96f9f4
MAJD ABOU HASHISH <104038767+MAJDABOUHASHISH@users.noreply.github.com>
2022-05-11 03:48:40 +0300

    Add files via upload

---
+++ ProjectsManager/src/CustomProjects.java
@@ -0,0 +1,17 @@
+public class CustomProjects {
+
+    private int id;
+    private String name;
+    private int issues;
+
+    public CustomProjects(int id, String name, int issues) {
+        this.id = id;
+        this.name = name;
+        this.issues = issues;
+    }
+
+    public int getId() {return id;}
+    public String getName() {return name;}
+    public int getIssues() {return issues;}
+
+}
